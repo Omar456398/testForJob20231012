546e774cd79ac5afc576c984be41309e6fca9fff
Omar456398 <omarraafatpnwcsh@gmail.com>
2023-10-13 15:34:10 +0300

    create scrolling effect

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
+jQuery.expr.filters.offscreen = function (element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.x + rect.width < 0 ||
+    rect.y + rect.height < 0 ||
+    rect.x > window.innerWidth ||
+    rect.y > window.innerHeight
+  );
+};
 $(document).ready(function () {
   let scrollorama = $.scrollorama({
     blocks: ".scrollblock",
   });
-  let bodyPartName = ''
+  let bodyPartName = "";
   let setBodyPart = (partName) => {
     bodyPartName = partName;
     [...document.getElementsByClassName("circle-thropping")].forEach((item) => {
-        if (partName === item.getAttribute('bodypart')) {
-          item.classList.add("visible");
-        } else {
-          item.classList.remove("visible");
-        }
-      });
-  } 
-  let onBodyPartClick = (event) => {
-    setBodyPart(event.target.parentElement.getAttribute('bodypart'))
+      if (partName === item.getAttribute("bodypart")) {
+        item.classList.add("visible");
+      } else {
+        item.classList.remove("visible");
+      }
+    });
+    const buttonsBodyparts = $(".body-parts-buttons").find("button");
+    buttonsBodyparts.each((index) => {
+      item = buttonsBodyparts[index].parentElement;
+      if (partName === item.getAttribute("bodypart")) {
+        item.classList.add("body-part-active");
+      } else {
+        item.classList.remove("body-part-active");
+      }
+    });
   };
-  $(".circle-inner").each((_) => {
-    $(this).click(onBodyPartClick);
+  let onBodyPartClick = (event) => {
+    const bodypart = event.target.parentElement.getAttribute("bodypart");
+    if(window.innerWidth <= 766) {
+    document.getElementById("body-part-" + bodypart).scrollIntoView();
+    } else {
+        setBodyPart(bodypart)
+    }
+  };
+
+  const circleInners = $(".circle-inner");
+  const buttonsBodyparts = $(".body-parts-buttons").find("button");
+  circleInners.each((index) => {
+    circleInners[index].onclick = onBodyPartClick;
+  });
+  buttonsBodyparts.each((index) => {
+    buttonsBodyparts[index].onclick = onBodyPartClick;
+  });
+  scrollorama.onBlockChange(function () {
+    const blockID =
+      scrollorama.settings.blocks.eq(scrollorama.blockIndex).attr("id") || "";
+    if (blockID.startsWith("body-part-") && window.innerWidth <= 766) {
+      $(".body-parts-container-inner")[0].classList.add("fixed");
+      $(".body-parts-container-inner")[0].classList.remove("finished-fix");
+      const bodyPartNameCurrent = blockID.substr("body-part-".length);
+      setBodyPart(bodyPartNameCurrent);
+    } else {
+      $(".body-parts-container-inner")[0].classList.remove("fixed");
+      if (blockID === "end-body-parts-fix" && window.innerWidth <= 766) {
+        $(".body-parts-container-inner")[0].classList.add("finished-fix");
+      }
+    }
   });
 });
